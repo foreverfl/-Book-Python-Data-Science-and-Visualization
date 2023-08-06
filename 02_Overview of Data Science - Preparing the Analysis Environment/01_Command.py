59eab11857d30fc71e6d96c503ee7b390cc93e85
foreverfl <forever_fl@naver.com>
2023-08-07 06:19:17 +0900

    changed chapter name

---
+++ 02_Overview of Data Science - Preparing the Analysis Environment/01_Command.py
@@ -0,0 +1,14 @@
+"""
+- conda 명령어
+* conda create –n (가상환경이름) python=(사용할 파이썬 버전)
+* conda info –envs
+* conda active (가상환경이름) / conda deactivate
+* conda install jupyter notebook
+* conda install (패키지이름)
+* pyton –m ipykernel install –user —name (가상환경명) --display –name “(표시 이름)”
+* jupyter notebook
+* conda create –n (신규 가상환경명) --clone (기존 가상환경명)
+* conda env remove –n(가상환경명)
+* conda env export > (가상환경명).yaml
+* conda env create –f(가상환경이름).yaml
+"""
