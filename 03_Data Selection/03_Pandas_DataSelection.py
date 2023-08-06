9b81f3377bddeae1307adfefde4655b94926bfdc
foreverfl <forever_fl@naver.com>
2023-08-07 06:31:27 +0900

    modified chapter03

--- 03_Data Selection/03_Pandas_DataSelection.py
+++ 03_Data Selection/03_Pandas_DataSelection.py
@@ -13,12 +13,10 @@ df = pd.DataFrame(data)
 # DataFrame 출력
 print(df)
 
-# 1. loc: 인덱스를 기준으로 해당하는 행의 데이터를 선택
-# 인덱스 2의 데이터를 선택하여 추출
-row_data = df.loc[2]
+# loc: 인덱스를 기준으로 해당하는 행의 데이터를 선택
+row_data = df.loc[2]  # 인덱스 2의 데이터를 선택하여 추출
 print(row_data)
 
-# 2. iloc: 행과 열의 위치를 기준으로 해당하는 데이터를 선택
-# 첫 번째 행의 첫 번째 열 데이터를 선택하여 추출
-cell_data = df.iloc[0, 0]
+# iloc(integer-location): 행과 열의 위치를 기준으로 해당하는 데이터를 선택
+cell_data = df.iloc[0, 0]  # 첫 번째 행의 첫 번째 열 데이터를 선택하여 추출
 print(cell_data)
